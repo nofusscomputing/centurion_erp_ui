3df7290d49afeafa6f5c8f5840863941cdf656f2
Jon <jonathon.lockwood@networkedweb.com>
2024-09-27 02:59:16 +0930

    feat(function): Render Markdown text
    ref: #1

---
+++ src/functions/RenderMarkdown.js
@@ -0,0 +1,50 @@
+import hljs from 'highlight.js'
+
+import markdownIt from "markdown-it";
+import { full as emoji } from 'markdown-it-emoji'
+
+
+
+const anchor = require('markdown-it-anchor').default
+
+const md = markdownIt({
+    linkify: true,
+    highlight: function (str, lang) {
+
+        if (lang && hljs.getLanguage(lang)) {
+
+            try {
+                return '<pre><code class="hljs">' +
+                    hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+                    '</code></pre>';
+            } catch (__) {}
+
+        }
+    
+        return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
+
+    }
+})
+
+    .use( require( 'markdown-it-admon' ) )
+
+    .use( anchor, { permalink: true} )
+
+    .use( emoji)
+
+    .use( require('markdown-it-footnote') )
+
+    .use( require('markdown-it-task-lists') );
+
+
+
+export default function RenderMarkdown(markdown) {
+
+    let rendered_markdown = md.render( String(markdown.children) )
+
+    return <div dangerouslySetInnerHTML={createHTML(rendered_markdown)} />;
+}
+
+function createHTML(html_string) {
+    return {__html: html_string};
+  }
