06e44029bbf14aa48dd3038b45f400ff67b817ef
Jon <jonathon.lockwood@networkedweb.com>
2024-09-28 23:26:07 +0930

    feat(hook): Add getCookie to get a local session cookie by name
    ref: #1 nofusscomputing/centurion_erp#248 nofusscomputing/centurion_erp#307 nofusscomputing/centurion_erp#319

---
+++ src/hooks/getCookie.js
@@ -0,0 +1,37 @@
+/**
+ * Fetch the value of a cookie
+ * 
+ * @param {String} name Name of the Cookie to get
+ * @returns 
+ */
+export function getCookie(name) {
+
+    let value = null
+
+    if (document.cookie && document.cookie !== '') {
+
+        const cookies = document.cookie.split(';');
+
+        console.log(`found cookies (${cookies})`)
+
+        for (let i = 0; i < cookies.length; i++) {
+
+            const cookie = cookies[i].trim();
+
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+
+                value = decodeURIComponent(cookie.substring(name.length + 1));
+
+                break;
+
+            }
+
+        }
+
+    }
+
+    console.log(`Cookie Value (${value})`)
+
+    return null
+}
