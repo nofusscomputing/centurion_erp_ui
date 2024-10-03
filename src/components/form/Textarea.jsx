048cc06ba2a3375b7d6c82b6e014e503c55898a0
Jon <jonathon.lockwood@networkedweb.com>
2024-10-03 16:43:49 +0930

    feat(component): Add onChange and value to textarea field
    ref: #1 nofusscomputing/centurion_erp#248 nofusscomputing/centurion_erp#307 nofusscomputing/centurion_erp#319

--- src/components/form/Textarea.jsx
+++ src/components/form/Textarea.jsx
@@ -1,15 +1,28 @@
 const TextArea = ({
+    id,
     label,
     helptext=null,
     required=false,
-    error_text=null
+    error_text=null,
+    value= '',
+    onChange = null
 }) => {
+
+    if( value === null ) {
+        value = ''
+    }
 
     return (
         <fieldset>
             <label className="name">{label}</label>
             <span className="help-text">{helptext}</span>
-            <textarea required={required} className="common-field"></textarea>
+            <textarea
+                id={id}
+                required={required}
+                className="common-field"
+                onChange={onChange}
+    
+            >{value}</textarea>
             <span className="error-text">{error_text}</span>
         </fieldset>
      );
