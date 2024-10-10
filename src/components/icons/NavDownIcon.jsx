af1f6edd7ce2c5b0dd0e458617bb73b02b69ca9c
Jon <jonathon.lockwood@networkedweb.com>
2024-10-11 02:01:12 +0930

    feat(component): Add class to nav down icon
    ref: #1 nofusscomputing/centurion_erp#248 nofusscomputing/centurion_erp#307 nofusscomputing/centurion_erp#319

--- src/components/icons/NavDownIcon.jsx
+++ src/components/icons/NavDownIcon.jsx
@@ -1,11 +1,12 @@
 const NavDownIcon = ({
     width = '20px',
     height = '20px',
-    fill = '#FFF'
+    fill = '#FFF',
+    class_name = null
 }) => {
 
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width={ width } height={ height } fill={ fill }>
+        <svg className={class_name} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width={ width } height={ height } fill={ fill }>
             <path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z"/>
         </svg>
     );
