c0b9403d663b91897a618592f893b226e43f29ae
Jon <jonathon.lockwood@networkedweb.com>
2024-11-30 17:13:50 +0930

    feat(component): Add UI release details to the footer
    ref: #29 #33 nofusscomputing/centurion_erp#410

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -43,12 +43,39 @@ const Footer = ({
         api_version_data
     ])
 
+    let ui_version = 'UI release: '
+
+    if( window.env.CI_COMMIT_TAG ) {
+
+        ui_version += window.env.CI_COMMIT_TAG
+
+    }else{
+
+        ui_version += 'development'
+    }
+
+    if( window.env.CI_COMMIT_SHA ) {
+
+        if( window.env.CI_PROJECT_URL ) {
+
+            const sha_url = window.env.CI_PROJECT_URL + '/commit/' + window.env.CI_COMMIT_SHA
+
+            ui_version = (
+                <>
+                {ui_version} (<a href={sha_url} target="_blank">{window.env.CI_COMMIT_SHA}</a> )
+                </>
+            )
+
+        }
+
+    }
+
 
     return ( 
         <footer>
             <div className="column left">&nbsp;</div>
             <div className="column center">Centurion ERP brought to you by <a href="https://nofusscomputing.com" target="new">No Fuss Computing</a></div>
-            <div className="column right">{api_version}</div>
+            <div className="column right">{api_version}<br />{ui_version}</div>
         </footer>
      );
 }
