f2cbcbb8d190cf6742e3be807841f7d88b6c4ee0
Jon <jonathon.lockwood@networkedweb.com>
2024-12-20 17:12:19 +0930

    fix(layout): use `back` url not `return_url` if it exists
    ref: #34

--- src/layout/ModelForm.jsx
+++ src/layout/ModelForm.jsx
@@ -192,8 +192,8 @@ const ModelForm = ({
 
                     if ( response.ok ) {
 
-                        navigate(metadata.urls.return_url ?
-                            String(metadata.urls.return_url).split('api/v2')[1]
+                        navigate(metadata.urls.back ?
+                            String(metadata.urls.back).split('api/v2')[1]
                             : String(metadata.urls.self).split('api/v2')[1])
 
                     } else {
