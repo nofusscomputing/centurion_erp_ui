6c19e9d2ed620ddb9876289e1a83844e9c3c7b40
Jon <jonathon.lockwood@networkedweb.com>
2025-01-20 00:25:31 +0930

    feat(component): Markdown editor
    ref: #42 #45

---
+++ src/components/MarkdownEditor.jsx
@@ -0,0 +1,122 @@
+import TextArea from "./form/Textarea";
+import Button from "./form/Button";
+import { useState } from "react";
+import RenderMarkdown from "../functions/RenderMarkdown";
+
+/**
+ * Markdown Editor with preview tab.
+ * 
+ * @param {String} field_name The name of the field as fetched from the API.
+ * @param {Object} Metadata The metadata page from the api with the field information.
+ * @param {String} value The markdown text to display within the field
+ * @param {Function} onCancel The function to run when the cancel button is clicked
+ * @param {Function} onSave The function to run when the save button is clicked 
+ * @returns 
+ */
+const MarkdownEditor = ({
+    field_name = null,
+    metadata = null,
+    markdown = null,
+    onCancel = null,
+    onSave = null,
+}) => {
+
+    const [ editing, setEditing ] = useState( true )
+
+    const [form_data, setFormData ] = useState({ 'description': markdown })
+
+
+    const handleChange = (e) => {
+
+        setFormData({'description': e.target.value})
+
+    }
+
+    const handleSave = (e) => {
+
+        onSave({
+            'event': e,
+            ...form_data
+        })
+
+    }
+
+
+    const handletabClick = (e) => {
+
+        if( String(e.target.innerText).toLowerCase() === 'preview' ) {
+
+            e.target.classList.add("active")
+            e.target.parentElement.children['tab-edit'].classList.remove("active")
+
+            setEditing(false)
+
+        }else if( String(e.target.innerText).toLowerCase() === 'edit' ) {
+
+            e.target.classList.add("active")
+            e.target.parentElement.children['tab-preview'].classList.remove("active")
+
+            setEditing(true)
+
+        }
+    }
+
+
+    console.debug(JSON.stringify(form_data))
+
+    return (
+        <div
+            style={{
+                display: 'block',
+                width: '100%'
+            }}
+        >
+            <div id="tabs" className="description-edit tabs">
+                <span id="tab-edit" className="tab active" onClick={handletabClick}>Edit</span>
+                <span id="tab-preview" className="tab" onClick={handletabClick}>Preview</span>
+            </div>
+
+            <div className="md-editor">
+
+                {editing &&
+                <>
+                <TextArea
+                    auto_content_height = {true}
+                    class_name ={'full-width'}
+                    id = {field_name}
+                    fieldset = {false}
+                    field_data = {metadata.fields[field_name]}
+                    value = {form_data[field_name]}
+                    onChange = {handleChange}
+                />
+                </>
+                }
+
+                { ! editing &&
+                    <>
+                    <div className="markdown">
+                        <RenderMarkdown full_width={true} env={metadata.fields[field_name].render ?? {}}>
+                            {form_data[field_name]}
+                        </RenderMarkdown>
+                    </div>
+                    </>
+                }
+            </div>
+            <div>
+                <Button
+                    button_text = 'Cancel'
+                    type = 'cancel'
+                    buttonClickCallback = {onCancel}
+                />
+                <Button
+                    button_text = 'Save'
+                    buttonClickCallback = {handleSave}
+                />
+            </div>
+        </div>
+        
+    );
+
+}
+ 
+export default MarkdownEditor;
