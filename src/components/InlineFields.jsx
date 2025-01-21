f08e88d14763d9e367998529cea872ceb66edd44
Jon <jonathon.lockwood@networkedweb.com>
2025-01-21 20:39:56 +0930

    feat(component): Inline form field editor
    ref: #42 #46

---
+++ src/components/InlineFields.jsx
@@ -0,0 +1,244 @@
+import { useState } from "react";
+
+import FieldData from "../functions/FieldData";
+import Select from "./form/Select";
+import { apiFetch } from "../hooks/apiFetch";
+import Button from "./form/Button";
+import { Form, useParams, useRouteError } from "react-router";
+
+
+
+/**
+ * Inline Editable field
+ *
+ * Supported API field Type / Form field types suppored are:
+ *
+ * - Choice / Select (Single and multiselect)
+ *
+ * - Relationship / Select (Single and multiselect)
+ *
+ * @param {Object} data API data containing the field
+ * @param {String} field_name Name of the field
+ * @param {Object} metadata API Metadata for the field
+ *
+ * @returns Inline Field
+ */
+const InlineField = ({
+    data,
+    field_name,
+    metadata,
+}) => {
+
+    let sanitized_field_name = field_name
+
+    if( String(field_name).endsWith('_badge') ) {
+
+        sanitized_field_name = String(field_name).replace('_badge', '')
+
+    }
+
+    const [ editing, setEditing ] = useState(false)
+
+
+    const handleEditClick = (e) => {
+
+        setEditing((prevState) => {
+
+            let editVal = prevState ? false : true
+            
+            return editVal
+        })
+    }
+
+
+    const handleFormSubmit = (e) => {
+
+        setEditing(false)
+
+    }
+
+
+    console.debug(`InlineField: ${JSON.stringify(data)}`)
+
+
+    const fetchFormField = () => {
+
+
+        switch( metadata.fields[sanitized_field_name].type ) {
+
+            case 'Choice':
+            case 'Relationship':
+
+                return (<Select
+                    id = {sanitized_field_name}
+                    value={data[sanitized_field_name]}
+                    field_data={metadata.fields[sanitized_field_name]}
+                    field_only={true}
+                />)
+        }
+    }
+
+
+    return (
+        <fieldset>
+            <Form method="patch" action={String(data._urls._self).split('api/v2')[1]} onSubmit={handleFormSubmit}>
+            <label>
+                {metadata.fields[field_name].label}
+                { ! metadata.fields[sanitized_field_name].read_only &&
+                <span
+                    id={'edit-field-' + field_name}
+                    onClick={handleEditClick}
+                    style={{
+                        color: 'var(--contrasting-colour)',
+                        float: 'right',
+                        fontSize: 'smaller',
+                        fontWeight: 'normal',
+                        paddingRight: '10px;'
+                    }}
+                >
+                    Edit
+                </span>}
+            </label>
+
+            { editing &&
+
+            fetchFormField()
+
+            }
+
+            { editing && 
+                <>
+                <input id="id" type="hidden"  name="id" value={data['id']} />
+                <input id="metadata" type="hidden"  name="metadata" value={JSON.stringify(metadata)} />
+                <Button
+                    button_align = 'right'
+                    button_class = 'mini inverse'
+                    button_text = 'Save'
+                />
+                </>
+            }
+
+            { ! editing &&
+
+                <span className="text">
+                    <FieldData
+                        metadata = {metadata}
+                        field_name = {field_name}
+                        data = {data}
+                    />
+                </span>
+
+            }
+            </Form>
+        </fieldset>
+    );
+}
+ 
+export default InlineField;
+
+export async function InlineFieldAction({
+    request,
+    params,
+}) {
+
+    if( ! String(request.url).endsWith(document.location.pathname) ) {    // as request does not contain the path, check doc path
+
+        throw Error(`InlineFieldAction URL ${request.url} does not match ${document.location.pathname}`)
+    }
+
+    const data = await request.formData()
+
+    const metadata = JSON.parse(data.get('metadata'))
+
+    let form_data = {}
+
+    for (const itItem of data.entries()) {
+
+        if( itItem[0] === 'metadata' ) {
+
+            continue;
+        }
+
+        console.debug(`InlineFieldAction=:${itItem}`);
+
+        if( ! metadata.fields.hasOwnProperty(itItem[0]) ) {    // field not part of request
+
+            continue;
+        }
+
+        let value = ''
+
+        switch( String(metadata.fields[itItem[0]].type).toLowerCase() ) {
+
+            case 'boolean':
+
+                value = Boolean(itItem[1]);
+
+                break;
+
+            case 'choice':
+            case 'integer':
+
+                value = Number(itItem[1]);
+
+                break;
+
+            case 'relationship':
+
+                if( form_data.hasOwnProperty( itItem[0] ) ) {
+
+                    value = [ ...form_data[itItem[0]], Number(itItem[1]) ]
+
+                } else {
+
+                    value = [ Number(itItem[1]) ]
+
+                    if( typeof(itItem[1]) === 'array' ) {
+
+                        value = Number(itItem[1])
+
+                    }
+                }
+
+                break;
+
+            case 'string':
+
+                value = String(itItem[1])
+
+                break;
+
+            default:
+
+                value = itItem[1];
+
+                break;
+
+        }
+
+        form_data = {
+            ...form_data,
+            [itItem[0]]: value
+        }
+
+        console.debug(`InlineFieldAction (json apend): ${JSON.stringify(form_data)}`)
+
+      }
+
+    console.debug(`InlineFieldAction (json): ${JSON.stringify(form_data)}`)
+
+    const update = await apiFetch(
+        document.location.pathname,
+        (data) => {
+
+            // onUpdated(data)
+
+        },
+        request.method,
+        form_data
+    )
+
+    return null;
+
+}
+
