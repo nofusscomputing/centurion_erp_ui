4753d23a547132b778d3af6ad9a09e54634400a3
Jon <jonathon.lockwood@networkedweb.com>
2025-02-26 02:03:54 +0930

    feat(layour): Populate initial value for textfield field on ModelForm
    ref: #82 closes #85 closes nofusscomputing/centurion_erp#649

--- src/layout/ModelForm.jsx
+++ src/layout/ModelForm.jsx
@@ -325,6 +325,7 @@ const ModelForm = () => {
                                                 helptext   = {metadata.fields[field_key].help_text}
                                                 error_text = {form_error && form_error[field_key]}
                                                 required   = {metadata.fields[field_key].required}
+                                                initial = {metadata.fields[field_key]?.initial ? metadata.fields[field_key].initial : null }
                                                 value={value}
                                                 onChange={handleChange}
                                             />)
