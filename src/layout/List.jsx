91972abbd58a2e00469eb57828ef11c92e16b7ce
Jon <jonathon.lockwood@networkedweb.com>
2025-04-04 10:59:57 +0930

    refactor(layout): use url._self for list data url
    ref: #97 nofusscomputing/centurion_erp#705

--- src/layout/List.jsx
+++ src/layout/List.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData, useParams } from "react-router"
 
 import Table from "../components/Table"
-import urlBuilder from "../hooks/urlBuilder";
 import { useEffect, useState } from "react";
 import ContentHeader from "../components/page/ContentHeader";
 
@@ -13,9 +12,6 @@ const List = () => {
     const [ content_header_icon, SetContentHeaderIcon ] = useState(null)
 
     const params = useParams();
-    const url_builder = urlBuilder(
-        params
-    )
 
     const {metadata, page_data} = useLoaderData();
 
@@ -25,17 +21,18 @@ const List = () => {
             content_heading={content_heading}
             content_header_icon={content_header_icon}
         />
+        { metadata &&
         <section>
             <div className="content">
                 <Table
                     callback={setContentHeading}
-                    data_url_path={url_builder.api.path}
+                    data_url_path={metadata.urls.self}
                     SetContentHeaderIcon = {SetContentHeaderIcon}
                     loader_metadata = {metadata}
                     loader_data = {page_data}
                 />
             </div>
-        </section>
+        </section>}
         </>
     );
 }
