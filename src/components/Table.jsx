9bc2263629de197bc3fab8418203eafadb941123
Jon <jonathon.lockwood@networkedweb.com>
2026-03-07 23:44:06 +0930

    chore(component): Limit per page options to 10
    this is what the current feature set has.
    
    ref: #129 #128

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -212,6 +212,12 @@ const DisplayTable = ({
                 itemCount={table_data.meta.pagination.count}
                 widgetId="bottom-pagination"
                 perPage={perPageNumber}
+                perPageOptions={[
+                    { title: '10', value: 10 },
+                    // { title: '20', value: 20 },
+                    // { title: '50', value: 50 },
+                    // { title: '100', value: 100 }
+                ]}
                 page={pageNumber}
                 variant={PaginationVariant.bottom}
                 onSetPage={onSetPage}
