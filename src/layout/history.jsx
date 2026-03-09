308c41ee4af6381ab53dd98427c741190f0ef2ff
Jon <jonathon.lockwood@networkedweb.com>
2026-03-09 19:02:47 +0930

    refactor(layout): Update HistoryView so its using PatternFly
    ref: #141 #140

--- src/layout/history.jsx
+++ src/layout/history.jsx
@@ -1,38 +1,43 @@
+import { useState } from "react";
+
 import { useParams } from "react-router"
 
+import {
+    PageGroup,
+    PageSection
+} from "@patternfly/react-core";
+
+import ContentHeader from "../components/page/ContentHeader";
 import DisplayTable from "../components/DisplayTable"
 import urlBuilder from "../hooks/urlBuilder";
-import { useState } from "react";
-import ContentHeader from "../components/page/ContentHeader";
 
 
 
 const History = () => {
 
-    const [ content_heading, setContentHeading ] = useState(null)
-    const [ content_header_icon, SetContentHeaderIcon ] = useState(null)
+    const [ content_heading, setContentHeading ] = useState(null);
 
     const params = useParams();
 
     const url_builder = urlBuilder(
         params
-    )
+    );
 
     return (
-        <>
+        <PageGroup>
         <ContentHeader
             content_heading={content_heading}
-            content_header_icon={content_header_icon}
         />
-        <section>
-            <div className="content">
+            <PageSection
+                aria-labelledby="page-content"
+                isFilled={true}
+            >
                 <DisplayTable
                     callback={setContentHeading}
                     data_url_path={url_builder.params.module + '/' + url_builder.params.model + '/' + url_builder.params.pk + '/history'}
                 />
-            </div>
-        </section>
-        </>
+            </PageSection>
+        </PageGroup>
     );
 }
 
