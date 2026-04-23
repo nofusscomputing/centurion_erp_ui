ba0a6f357709892fa6c3c1c47f2ca03455a91f4a
Jon <jonathon.lockwood@networkedweb.com>
2026-04-23 12:56:20 +0930

    chore(mock): Add mock for window.MatchMedia
    ref: #167 #165

--- jest.config.js
+++ jest.config.js
@@ -168,7 +168,10 @@ const config = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    '<rootDir>/src/__test__/__mocks__/fetch.js'
+    '<rootDir>/src/__test__/__mocks__/apiFetch.js',
+    '<rootDir>/src/__test__/__mocks__/fetch.js',
+    '<rootDir>/src/__test__/__mocks__/reactRouter.js',
+    '<rootDir>/src/__test__/__mocks__/windowMatchMedia.js'
   ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
