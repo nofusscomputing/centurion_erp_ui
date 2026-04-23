531f0b785f8af1d7a20364de2937c208939f0b00
Jon <jonathon.lockwood@networkedweb.com>
2026-04-23 12:04:34 +0930

    chore(jest): allow patternfly for transforms
    ref: #167 #165

--- jest.config.js
+++ jest.config.js
@@ -212,8 +212,7 @@ const config = {
   transform: {
         "\\.[jt]sx?$": "babel-jest",
         "\\.mjs$": "babel-jest",
-        '\\.svg$':
-        '<rootDir>/src/__test__/__mocks__/svgPathMock.js',
+        '\\.svg$': '<rootDir>/src/__test__/__mocks__/svgPathMock.js',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
@@ -222,7 +221,7 @@ const config = {
     // "src/",
     //   "\\.pnp\\.[^\\/]+$"
     // "node_modules/(?!src)/'",
-    "/node_modules/(?!(markdown-it)/)"
+    "/node_modules/(?!(markdown-it|react-router|@patternfly/react-core)/)"
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
