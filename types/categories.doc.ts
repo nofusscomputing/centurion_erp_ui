4c03926474c3ab4e015cb0041b5fbc2889720129
Jon <jonathon.lockwood@networkedweb.com>
2026-05-08 17:12:09 +0930

    docs(api): start on backend API objects
    ref: #172 #149

---
+++ types/categories.doc.ts
@@ -0,0 +1,10 @@
+/**
+ * @categoryDescription Backend
+ * 
+ * Items under this category are directly related to the backend.
+ * 
+ * Where there may be multiple definitions defined, consider what is under this
+ * category as the single-source-of-truth.
+ * 
+ * @module
+ */
