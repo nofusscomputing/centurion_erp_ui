a537f76ea15426463c69b8e0cbd58c92bfa6f495
Jon <jonathon.lockwood@networkedweb.com>
2026-07-26 14:48:31 +0930

    chore(test): Update test expectations for rendering markdown model and tickets
    was earlier code update that changed icon style
    
    ref: #209 #205

--- src/functions/__test__/unit/RenderMarkdown.test.js
+++ src/functions/__test__/unit/RenderMarkdown.test.js
@@ -1275,8 +1275,8 @@ describe("Plugins - JSX Objects", () => {
                         '<a href="/device/1">'+
                             '<span class="badge-icon icon">'+
                                 '<span class="pf-v6-c-icon">'+
-                                    '<span class="pf-v6-c-icon__content">'+
-                                        '<span>'+
+                                    '<span class="pf-v6-c-icon__content pf-m-xl">'+
+                                        '<span title="device">'+
                                             '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className="pf-v6-svg" fill="currentColor" height="auto" role="img" width="auto">'+
                                                 '<path d="M48-144v-72h864v72H48Zm120-120q-29.7 0-50.85-21.15Q96-306.3 96-336v-408q0-29.7 21.15-50.85Q138.3-816 168-816h624q29.7 0 50.85 21.15Q864-773.7 864-744v408q0 29.7-21.15 50.85Q821.7-264 792-264H168Zm0-72h624v-408H168v408Zm0 0v-408 408Z">'+
                                                 '</path>'+
@@ -1317,13 +1317,15 @@ describe("Plugins - JSX Objects", () => {
                     'a ticket reference is numerical and prefixed with a hash. ' +
                     '<span class="text-inline">' +
                         '<span class="badge-icon icon ticket-status-icon ticket-status-icon-new">' +
-                            '<span class="pf-v6-c-icon">' +
-                                '<span class="pf-v6-c-icon__content">' +
-                                    '<span>' +
-                                        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className="pf-v6-svg" fill="currentColor" height="auto" role="img" width="auto">' +
-                                            '<path d=\"M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z">' +
-                                            '</path>' +
-                                        '</svg>' +
+                            '<span class="pf-v6-c-icon pf-m-lg">' +
+                                '<span class="pf-v6-c-icon__content pf-m-xl">' + 
+                                    '<span title="ticket_status_new">' +
+                                        // '<span>' +
+                                            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className="pf-v6-svg" fill="currentColor" height="auto" role="img" width="auto">' +
+                                                '<path d=\"M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z">' +
+                                                '</path>' +
+                                            '</svg>' +
+                                        // '</span>' +
                                     '</span>' +
                                 '</span>' +
                             '</span>' +
@@ -1346,7 +1348,7 @@ describe("Plugins - JSX Objects", () => {
                 "tickets": {
                     "1": {
                         "status": "new",
-                        "ticket_type": "request",
+                        "type": "request",
                         "title": "a name",
                         "url": "/ticket/1"
                     }
