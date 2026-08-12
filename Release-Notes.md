580bf11cf4e475c7d898f69544a8548739536b16
Jon <jonathon.lockwood@networkedweb.com>
2026-08-12 18:58:12 +0930

    feat(function): Add HTTP base fetcher
    ref: #221 #220

--- Release-Notes.md
+++ Release-Notes.md
@@ -1,1 +1,5 @@
 ## v0.13.0
+
+    - Begin refactor of fetching api data. This move is towards creating data/backend specific fetchers.
+
+        - HTTP Base Fetcher added
--- src/functions/http.ts
+++ src/functions/http.ts
@@ -1,3 +1,8 @@
+import {
+    HTTPNotAuthenticated
+} from "../classes/Exceptions";
+
+
 
 /**
  * Named parameters for a HTTP request.
@@ -7,6 +12,23 @@
  * @since 0.13.0
  */
 export interface HTTPNamedParams {
+
+    /**
+     * Body to use for request.
+     */
+    body?: RequestInit["body"] | null,
+
+    /**
+     * Send Credentials with request.
+     * 
+     * When set to `true` credentials will be set to `same-orign`
+     */
+    credentials?: boolean
+
+    /**
+     * Headers to add to the request.
+     */
+    headers?: RequestInit["headers"],
 
     /**
      * HTTP method to use for the request.
@@ -19,19 +41,78 @@ export interface HTTPNamedParams {
     url: string,
 }
 
+
+
 /**
  * This object is intended to serve as the single point for all HTTP requests
  * that the UI will make.
  * 
+ * When calling this fetcher, ensure that you have provided everything required
+ * to make the request.
+ * 
+ * ``` js
+ * 
+ * const myRequest = httpRequest({
+ *      url: "https://my-domain-name.tld/some-path",
+ *      headers: {
+ *          "Content-Type": "application/json"
+ *      },
+ *      body: {
+ *          "a_key": "a value"
+ *      }
+ * })
+ * 
+ * ```
+ * 
+ * @summary HTTP fetcher.
+ * 
  * @category fetcher
  * @expandType HTTPNamedParams
  * @since 0.13.0
+ * @throws {@link HTTPNotAuthenticated} HTTP/401 was returned.
  */
-export default async function httpRequest({
+export async function httpRequest({
+    body = null,
+    credentials = false,
+    headers = {},
     method = "GET",
     url,
 }: HTTPNamedParams): Promise<Response> {
 
-    return
+
+    let options: RequestInit = {
+        // ...( credentials ? { credentials: 'same-origin' } : {} ),
+        ...( credentials ? { credentials: 'include' } : {} ),
+        headers: {
+            ...headers,
+            // "X-Request-ID": crypto.randomUUID()
+        },
+        method: method,
+        // mode: 
+    };
+
+
+    if( body ) {
+        options['body'] = body;
+    }
+
+
+    const Request = await fetch(
+        url,
+        options
+    ).then(response => {
+
+        if( response.status === 401 ) {
+
+            throw new HTTPNotAuthenticated( response )
+
+        }
+
+        return response
+
+    });
+
+
+    return Request;
 
 }
