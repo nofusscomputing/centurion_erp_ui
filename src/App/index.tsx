851796ff52867454f562cbde4728f5526647a816
Jon <jonathon.lockwood@networkedweb.com>
2026-08-15 21:11:56 +0930

    refactor: move app loading to sub-dir
    ref: #228 #227 #174

---
+++ src/App/index.tsx
@@ -0,0 +1,30 @@
+import {
+    useMemo
+} from "react";
+
+import {
+    RouterProvider
+} from "react-router/dom";
+
+import dynamicRouter from "./router";
+
+
+
+/**
+ * Adds the routes provided by {@link dynamicRouter}
+ * 
+ * @summary App Entrypoint
+ * 
+ * @category Other
+ * @since 0.13.0
+ */
+function App() {
+
+    const router = useMemo( () => dynamicRouter(),[] );
+
+    return (
+        <RouterProvider router={router} />
+    );
+}
+
+export default App;
